0feee7626f45f0bc5bd9c372aa4b27fd825ca87b
ashrafulasif123 <cseasifiiuc9@gmail.com>
2023-05-02 10:59:24 +0600

    package json

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const chef = require('./data/chef.json');
+
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('chef is cooking');
+})
+
+app.get('/chef', (req, res) => {
+    res.send(chef);
+})
+
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const selectedChef = chef.find(c => c.id === id);
+    res.send(selectedChef);
+})
+
+app.listen(port, () => {
+    console.log(`chef is cooking in: ${port}`)
+})
